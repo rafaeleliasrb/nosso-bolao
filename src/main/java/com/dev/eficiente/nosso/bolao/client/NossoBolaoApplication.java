fcb1676056545f99f0d5c682cbca28d8a1290a2e
Rafael Elias <13rafael.elias@gmail.com>
2020-01-20 23:23:49 -0300

    First commit

---
+++ src/main/java/com/dev/eficiente/nosso/bolao/client/NossoBolaoApplication.java
@@ -0,0 +1,13 @@
+package com.dev.eficiente.nosso.bolao.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoBolaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoBolaoApplication.class, args);
+	}
+
+}
